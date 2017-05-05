1972c23d5340f7708ca2e382de6754982595c73e
Christian Otto <Christian.Otto@havox-design.de>
2017-05-06 01:06:22 +0200

    * Added tests for set duration functionality and error handling.

--- times-model-impl/src/test/java/net/havox/times/model/times/impl/WorkUnitDurationImplTest.java
+++ times-model-impl/src/test/java/net/havox/times/model/times/impl/WorkUnitDurationImplTest.java
@@ -17,9 +17,16 @@
 package net.havox.times.model.times.impl;
 
 import java.lang.reflect.Field;
+import java.time.Duration;
+import java.time.LocalDateTime;
 
 import net.havox.times.model.impl.AbstractChangeAwareClass;
 import net.havox.times.model.impl.AbstractChangeAwareClassTest;
+import net.havox.times.model.times.api.WorkUnitDuration;
+
+import org.junit.Test;
+
+ import static  org.junit.Assert.*;
 
 public class WorkUnitDurationImplTest extends AbstractChangeAwareClassTest
 {
@@ -39,5 +46,122 @@ public class WorkUnitDurationImplTest extends AbstractChangeAwareClassTest
     versionField.set( instance, version );
 
     return ( WorkUnitDurationImpl ) instance;
+  }
+  
+  private void processTestSetDuration( LocalDateTime startTime, LocalDateTime endTime, Duration expectedDuration ) {
+    WorkUnitDuration objectUnderTest = new WorkUnitDurationImpl();
+    
+    assertNotNull( objectUnderTest );
+    
+    objectUnderTest.setDuration( startTime, endTime );
+    
+    assertEquals( expectedDuration, objectUnderTest.getDuration() );
+  }
+  
+  private void processTestSetDuration( LocalDateTime startTime, Duration duration, Duration expectedDuration ) {
+    WorkUnitDuration objectUnderTest = new WorkUnitDurationImpl();
+    
+    assertNotNull( objectUnderTest );
+    
+    objectUnderTest.setDuration( startTime, duration );
+    
+    assertEquals( expectedDuration, objectUnderTest.getDuration() );
+  }
+  
+  @Test
+  public void testSetDurationLocalDateTimeLocalDateTimeNoException() {
+    LocalDateTime startTime = LocalDateTime.now();
+    Duration expectedDuration = Duration.ofMinutes( 23 * 60 + 59 );
+    LocalDateTime endTime = startTime.plus(expectedDuration );
+    
+    assertNotNull( startTime );
+    assertNotNull( endTime );
+    
+    processTestSetDuration( startTime, endTime, expectedDuration );
+  }
+  
+  @Test(expected = IllegalArgumentException.class)
+  public void testSetDurationLocalDateTimeLocalDateTimeFirstParameterNull() {
+    LocalDateTime startTime = LocalDateTime.now();
+    Duration expectedDuration = Duration.ofMinutes( 23 * 60 + 59 );
+    LocalDateTime endTime = startTime.plus(expectedDuration );
+    startTime = null;
+    
+    assertNull( startTime );
+    assertNotNull( endTime );
+    
+    processTestSetDuration( startTime, endTime, expectedDuration );
+  }
+  
+  @Test(expected = IllegalArgumentException.class)
+  public void testSetDurationLocalDateTimeLocalDateTimeSecondParameterNull() {
+    LocalDateTime startTime = LocalDateTime.now();
+    Duration expectedDuration = Duration.ofMinutes( 23 * 60 + 59 );
+    LocalDateTime endTime = null;
+    
+    assertNotNull( startTime );
+    assertNull( endTime );
+    
+    processTestSetDuration( startTime, endTime, expectedDuration );
+  }
+  
+  @Test(expected = IllegalArgumentException.class)
+  public void testSetDurationLocalDateTimeLocalDateTimeBothParametersNull() {
+    LocalDateTime startTime = null;
+    Duration expectedDuration = Duration.ofMinutes( 23 * 60 + 59 );
+    LocalDateTime endTime = null;
+    
+    assertNull( startTime );
+    assertNull( endTime );
+    
+    processTestSetDuration( startTime, endTime, expectedDuration );
+  }
+  
+  @Test
+  public void testSetDurationLocalDateTimeDurationNoException() {
+    LocalDateTime startTime = LocalDateTime.now();
+    Duration duration = Duration.ofMinutes( 23 * 60 + 59 );
+    Duration expectedDuration = duration;
+    
+    assertNotNull( startTime );
+    assertNotNull( duration );
+    
+    processTestSetDuration( startTime, duration, expectedDuration );
+  }
+  
+  @Test(expected = IllegalArgumentException.class)
+  public void testSetDurationLocalDateTimeDurationFirstParameterNull() {
+    LocalDateTime startTime = null;
+    Duration duration = Duration.ofMinutes( 23 * 60 + 59 );
+    Duration expectedDuration = Duration.ZERO;
+    
+    assertNull( startTime );
+    assertNotNull( duration );
+    
+    processTestSetDuration( startTime, duration, expectedDuration );
+  }
+  
+  @Test(expected = IllegalArgumentException.class)
+  public void testSetDurationLocalDateTimeDurationSecondParameterNull() {
+    LocalDateTime startTime = LocalDateTime.now();
+    Duration duration = null;
+    Duration expectedDuration = Duration.ZERO;
+    
+    assertNotNull( startTime );
+    assertNull( duration );
+    
+    processTestSetDuration( startTime, duration, expectedDuration );
+  }
+  
+  @Test(expected = IllegalArgumentException.class)
+  public void testSetDurationLocalDateTimeDurationBothParametersNull() {
+    LocalDateTime startTime = null;
+    Duration duration = null;
+    Duration expectedDuration = Duration.ZERO;
+    
+    assertNull( startTime );
+    assertNull( duration );
+    
+    processTestSetDuration( startTime, duration, expectedDuration );
   }
 }
