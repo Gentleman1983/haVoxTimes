10db621b0c875c348f8372ef5220a1ce39d83a74
Christian Otto <Christian.Otto@havox-design.de>
2017-07-05 19:07:00 +0200

    * Added random generator for model tests.

---
+++ times-model-api/src/test/java/net/havox/times/model/api/ModelRandomGenerator.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2017 [haVox] Design
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package net.havox.times.model.api;
+
+import java.security.SecureRandom;
+
+public class ModelRandomGenerator
+{
+
+  private static final SecureRandom RANDOM = new SecureRandom();
+
+  public static final String ALPHANUMERIC_STRING = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  public static final String ALPHABETIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  
+  private ModelRandomGenerator() {}
+
+  public static String randomString( int length )
+  {
+    return randomString( length, ALPHANUMERIC_STRING );
+  }
+
+  public static String randomString( int length, String alphabet )
+  {
+    StringBuilder builder = new StringBuilder( length );
+    for ( int i = 0; i < length; i++ )
+    {
+      builder.append( alphabet.charAt( RANDOM.nextInt( alphabet.length() ) ) );
+    }
+    return builder.toString();
+  }
+
+  public static int randomInt()
+  {
+    return RANDOM.nextInt();
+  }
+
+  public static int randomInt( int bound )
+  {
+    return RANDOM.nextInt( bound );
+  }
+  
+  public static int randomIntInRange( int min, int max) {
+    return randomInt( max - min + 1 ) + min;
+  }
+  
+  public static long randomLong()
+  {
+    return RANDOM.nextLong();
+  }
+
+  public static boolean randomBoolean()
+  {
+    return RANDOM.nextBoolean();
+  }
+  
+  public static double randomDouble()
+  {
+    return RANDOM.nextDouble();
+  }
+
+  public static float randomFloat()
+  {
+    return RANDOM.nextFloat();
+  }
+}
