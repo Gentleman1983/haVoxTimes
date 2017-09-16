266dc61c6ca73ee6f0139cc3ed90cc80cc7cb542
Christian Otto <Christian.Otto@havox-design.de>
2017-09-16 16:19:11 +0200

    * Removed unused import.

--- times-model-impl/src/main/java/net/havox/times/model/times/impl/EmploymentImpl.java
+++ times-model-impl/src/main/java/net/havox/times/model/times/impl/EmploymentImpl.java
@@ -24,7 +24,6 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import net.havox.times.model.contacts.api.Company;
